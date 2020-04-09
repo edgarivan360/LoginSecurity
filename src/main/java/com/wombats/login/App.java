d4481ca6a537fc74cb246f4b0412c21ddda38c1c
Edgar Quiroz <edgar_qp11@outlook.com>
2020-04-09 14:06:01 -0500

    Commit inicial

---
+++ src/main/java/com/wombats/login/App.java
@@ -0,0 +1,13 @@
+package com.wombats.login;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
